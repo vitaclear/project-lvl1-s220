5fd680ae371dbe9298f3c7356ee74196be92bc38
Vita Chistyakova <vita_avi@yahoo.com>
2018-01-24 14:40:31 +0300

    3rd step

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,5 @@
-#! /usr/local/bin/node
+#! /usr/bin/env node
 import hello from '../index';
-console.log ("Welcome to the Brain Games!");
+
+console.log('Welcome to the Brain Games!');
 hello();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync';
+
 const hello = () => {
-	const name = readlineSync.question ("Enter your name, please: ");
-	console.log ("Hello,", name, "!");
-}
+  const name = readlineSync.question('Enter your name, please: ');
+  console.log('Hello,', name, '!');
+};
 export default hello;
