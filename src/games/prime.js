edd93fa13b12270d70703893592deb8fde1085e8
Vita Chistyakova <vita_avi@yahoo.com>
2018-01-27 04:05:19 +0300

    Added game 'prime'

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,11 +4,11 @@ import { rules, process, randnum } from '../common';
 const primegame = () => {
   rules('Please answer "yes" if a number is prime, otherwise answer "no".');
   const isprime = (num) => {
-    if (num < 2) { return false; }
+    if (num < 2) { return 'no'; }
     for (let i = Math.round(num / 2); i > 1; i -= 1) {
-      if (num % i === 0) { return false; }
+      if (num % i === 0) { return 'no'; }
     }
-    return true;
+    return 'yes';
   };
   const makedouble = k => cons(k, isprime(k));
   process(() => makedouble(randnum(100)));
